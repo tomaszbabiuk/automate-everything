63b5a92d2bfe7d2d62cf10bee043505ccd2d2578
Tomasz Babiuk <tomasz.babiuk@gmail.com>
2020-01-20 21:46:45 +0100

    ui fixes

--- vuetify/src/views/Plugins.vue
+++ vuetify/src/views/Plugins.vue
@@ -1,34 +1,35 @@
 <template>
   <div>
     <v-card flat class="pa-5 mb-5" v-for="plugin in plugins" :key="plugin.name">
-      <v-layout row wrap justify-end>
-        <v-flex md8 sm12>
+      <v-row>
+        <v-col md="8" sm="12">
           <div class="caption grey--text">Name</div>
           <div>{{plugin.name}}</div>
-        </v-flex>
-        <v-flex md3 sm8>
+        </v-col>
+        <v-col md="1" sm="12">
           <div class="caption grey--text">Version</div>
           <div>{{plugin.version}}</div>
-        </v-flex>
-        <v-flex md1 sm4>
-          <v-chip v-if="plugin.enabled" outlined @click="disable" color="green">Enabled - click to disable</v-chip>
-          <v-chip v-else outlined @click="enable" color="red">Disabled - click to enable</v-chip>
-        </v-flex>
-        <v-flex md12 sm12>
+        </v-col>
+        <v-col md="3" sm="12" align="right">
+          <div class="caption grey--text">Enabled</div>
+          <v-chip v-if="plugin.enabled" outlined @click="enable" color="green">Yes</v-chip>
+          <v-chip v-else outlined @click="enable" color="red">No</v-chip>
+        </v-col>
+        <v-col md="12" sm="12">
           <div class="caption grey--text">Description</div>
           <div class="text-justify">{{plugin.description}}</div>
-        </v-flex>
-      </v-layout>
+        </v-col>
+      </v-row>
     </v-card>
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 
 export default {
   data: () => ({
-      plugins: null
+    plugins: null
   }),
   methods: {
     enable() {
@@ -36,17 +37,15 @@ export default {
     },
     disable() {
       alert("Disabling...");
-    },
+    }
   },
   mounted: function() {
-    this.plugins = localStorage.plugins
+    this.plugins = localStorage.plugins;
 
-    axios
-      .get('http://localhost/rest/plugins')
-      .then(response => {
-        this.plugins = response.data
-        localStorage.plugins = response
-      })  
+    axios.get("http://localhost/rest/plugins").then(response => {
+      this.plugins = response.data;
+      localStorage.plugins = response;
+    });
   }
 };
 </script>
