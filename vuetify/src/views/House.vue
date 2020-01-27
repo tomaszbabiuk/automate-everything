1c6c66162cf69b0a4efe01581f8a723ae434c100
Tomasz Babiuk <tomasz.babiuk@gmail.com>
2020-01-27 04:55:49 +0100

    configurables tree

---
+++ vuetify/src/views/House.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <v-treeview
+    dense
+    :items="configurables"
+    item-key="class"
+    item-text="class"
+  ></v-treeview>
+  </div>
+</template>
+
+<script>
+import { client } from "../rest.js"
+
+export default {
+  data: function() {
+    return {
+      items: [
+        { clazz: "name1", id: "id1" },
+        { clazz: "name2", id: "id2" },
+      ]
+    }
+  },
+  computed: {
+    configurables () {
+      return this.$store.state.configurables
+    }
+  },
+  mounted: function() {
+    client.getRootConfigurables()
+  }
+};
+</script>
