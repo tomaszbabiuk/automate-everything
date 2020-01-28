243c161b62efa76ee51e4d2e353765d4cfd41658
Tomasz Babiuk <tomasz.babiuk@gmail.com>
2020-01-28 21:59:38 +0100

    house tree

--- vuetify/src/views/House.vue
+++ vuetify/src/views/House.vue
@@ -1,33 +1,156 @@
 <template>
-  <div>
-    <v-treeview
-    dense
-    :items="configurables"
-    item-key="class"
-    item-text="titleRes"
-  ></v-treeview>
-  </div>
+  <v-row class="pa-4" justify="space-between">
+    <v-col cols="5">
+      <v-treeview
+        :active.sync="active"
+        :items="configurables"
+        item-key="class"
+        item-text="titleRes"
+        :open.sync="open"
+        activatable
+        color="warning"
+        open-on-click
+        transition
+      ></v-treeview>
+    </v-col>
+
+    <v-divider vertical></v-divider>
+
+    <v-col class="d-flex text-center">
+      <v-scroll-y-transition mode="out-in">
+        <div
+          v-if="!selected"
+          class="title grey--text text--lighten-1 font-weight-light"
+          style="align-self: center;"
+        >Select a User</div>
+        <div v-else>Selected</div>
+      </v-scroll-y-transition>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-import { client } from "../rest.js"
+import { client } from "../rest.js";
 
 export default {
   data: function() {
     return {
-      items: [
-        { clazz: "name1", id: "id1" },
-        { clazz: "name2", id: "id2" },
+      active: [],
+      open: [],
+      configurables: [
+        {
+          fields: [
+            {
+              name: "name",
+              hint: "Name",
+              class: "StringField"
+            },
+            {
+              name: "name",
+              hint: "Description",
+              class: "StringField"
+            }
+          ],
+          class: "SceneConfigurable",
+          addNewRes: "Scenes",
+          titleRes: "Scenes",
+          iconName: "scene",
+          children: []
+        },
+        {
+          fields: [
+            {
+              name: "name",
+              hint: "Name",
+              class: "StringField"
+            },
+            {
+              name: "name",
+              hint: "Description",
+              class: "StringField"
+            }
+          ],
+          class: "GroupConfigurable",
+          addNewRes: "Groups",
+          titleRes: "Groups",
+          iconName: "group",
+          children: []
+        },
+        {
+          fields: [
+            {
+              name: "name",
+              hint: "Name",
+              class: "StringField"
+            },
+            {
+              name: "name",
+              hint: "Description",
+              class: "StringField"
+            }
+          ],
+          class: "FloorConfigurable",
+          addNewRes: "House plan",
+          titleRes: "House plan",
+          iconName: "group",
+          children: [
+            {
+              fields: [
+                {
+                  name: "name",
+                  hint: "Name",
+                  class: "StringField"
+                },
+                {
+                  name: "name",
+                  hint: "Description",
+                  class: "StringField"
+                }
+              ],
+              class: "RoomConfigurable",
+              addNewRes: "Room",
+              titleRes: "Room",
+              iconName: "group",
+              children: []
+            }
+          ]
+        },
+        {
+          fields: [
+            {
+              name: "name",
+              hint: "Name",
+              class: "StringField"
+            },
+            {
+              name: "name",
+              hint: "Description",
+              class: "StringField"
+            }
+          ],
+          class: "DeviceConfigurable",
+          addNewRes: "Devices",
+          titleRes: "Devices",
+          iconName: "group",
+          children: []
+        }
       ]
-    }
+    };
   },
   computed: {
-    configurables () {
-      return this.$store.state.configurables
+    // configurables() {
+    //   return this.$store.state.configurables;
+    // },
+    selected() {
+      if (!this.active.length) return undefined;
+
+      const clazz = this.active[0];
+
+      return this.configurables.find(x => x.class === clazz);
     }
   },
   mounted: function() {
-    client.getConfigurables()
+    client.getConfigurables();
   }
 };
 </script>
