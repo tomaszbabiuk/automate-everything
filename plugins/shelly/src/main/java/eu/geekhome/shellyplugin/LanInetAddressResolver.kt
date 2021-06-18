e4b2497fc0f0dd4eed5612102bd41145a47dbf7e
Tomasz Babiuk <tomasz.babiuk@assaabloy.com>
2021-06-18 10:30:54 +0200

    resolving ip address in lan

--- plugins/shelly/src/main/java/eu/geekhome/shellyplugin/LanInetAddressResolver.kt
+++ plugins/shelly/src/main/java/eu/geekhome/shellyplugin/LanInetAddressResolver.kt
@@ -3,12 +3,10 @@ package eu.geekhome.shellyplugin
 import java.net.Inet4Address
 import java.net.InetAddress
 import java.net.NetworkInterface
-import java.util.*
 
 object LanInetAddressResolver {
 
     fun resolve(): InetAddress? {
-        val addresses: MutableList<InetAddress> = ArrayList()
         val interfaces = NetworkInterface.getNetworkInterfaces()
         while (interfaces.hasMoreElements()) {
             val cur = interfaces.nextElement()
@@ -16,7 +14,7 @@ object LanInetAddressResolver {
                 continue
             }
             for (interfaceAddress in cur.interfaceAddresses) {
-                if (interfaceAddress.broadcast == InetAddress.getByAddress(byteArrayOf(192.toByte(),168.toByte(),1.toByte(),255.toByte()))) {
+                if (interfaceAddress.broadcast.isSiteLocalAddress) {
                     val address = interfaceAddress.address
                     if (address is Inet4Address) {
                         return address
