2e1a6191135759745ecbac2602ef2341d059cc52
Tomasz Babiuk <tomasz.babiuk@assaabloy.com>
2021-09-28 22:02:58 +0200

    replacing "geekhome" with "automateeverything"

---
+++ plugins/gmail-action/src/main/java/eu/automateeverything/gmailactionplugin/R.kt
@@ -0,0 +1,40 @@
+package eu.automateeverything.bashactionplugin
+
+import eu.automateeverything.data.localization.Resource
+
+object R {
+    val field_command_hint = Resource(
+        "Bash command to execute",
+        "Komenda bash do wykonania"
+    )
+
+    val configurable_bash_script_action_title = Resource(
+        "Bash script",
+        "Skrypt bash"
+    )
+
+    val configurable_bash_script_action_description = Resource(
+        "Executes bash script",
+        "Uruchamia skrypt bash"
+    )
+
+    val configurable_bash_script_action_add = Resource(
+        "Add bash script action",
+        "Dodaj skrypt bash"
+    )
+
+    val configurable_bash_script_action_edit = Resource(
+        "Edit bash script",
+        "Edytuj skrypt bash"
+    )
+
+    val plugin_name = Resource(
+        "Gmail action",
+        "Akcja gmail"
+    )
+
+    val plugin_description = Resource(
+        "Sends an email using gmail",
+        "Wysyła email używając gmaila"
+    )
+}
