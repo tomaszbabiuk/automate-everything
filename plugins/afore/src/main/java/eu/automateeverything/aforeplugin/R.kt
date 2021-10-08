3fa52fae742711a1d2b5dd7b308c668f76add29c
Tomasz Babiuk <tomasz.babiuk@assaabloy.com>
2021-10-08 21:03:01 +0200

    Refactoring (less java and more kotlin)

---
+++ plugins/afore/src/main/java/eu/automateeverything/aforeplugin/R.kt
@@ -0,0 +1,12 @@
+package eu.automateeverything.aforeplugin
+
+import eu.automateeverything.data.localization.Resource
+
+object R {
+    var plugin_description = Resource(
+        "Unofficial support for Afore BNT008KTL inverter",
+        "Nieoficjalne wsparcie dla falownika Afore BNT008KTL"
+    )
+
+    var plugin_name = Resource("Afore", "Afore")
+}
