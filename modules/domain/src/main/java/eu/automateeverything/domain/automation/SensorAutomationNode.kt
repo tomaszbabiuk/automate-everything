f204803ba38bdde6ffa25318ecddd882e7af2735
Tomasz Babiuk <tomasz.babiuk@assaabloy.com>
2021-12-09 15:53:56 +0100

    moving sensors from "core" to "sensors"

--- modules/domain/src/main/java/eu/automateeverything/domain/automation/SensorAutomationNode.kt
+++ modules/domain/src/main/java/eu/automateeverything/domain/automation/SensorAutomationNode.kt
@@ -11,13 +11,8 @@ class SensorAutomationNode(
     override fun getValue(now: Calendar): PortValue? {
         if (deviceUnit.lastEvaluation.error != null) {
             val originName = deviceUnit.nameOfOrigin
-            if (originName != null) {
-                throw AutomationErrorException(R.error_other_device_failure(originName),
-                    deviceUnit.lastEvaluation.error!!)
-            } else {
-                throw AutomationErrorException(R.error_other_device_failure,
-                    deviceUnit.lastEvaluation.error!!)
-            }
+            throw AutomationErrorException(R.error_other_device_failure(originName),
+                deviceUnit.lastEvaluation.error!!)
         }
 
         return deviceUnit.lastEvaluation.value as PortValue?
