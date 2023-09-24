dc7c400ee5b082070533ae62d5783e6dc1ee2c07
Tomasz Babiuk <tomasz.babiuk@gmail.com>
2023-09-24 12:06:25 +0200

    Publish gradle for data, domain and interop

--- modules/interop/src/main/java/eu/automateeverything/interop/coap/VersionManifest.kt
+++
@@ -1,6 +0,0 @@
-package eu.automateeverything.interop.coap
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class VersionManifest(val versionMajor: Int, val versionMinor: Int, val uniqueId: String)
